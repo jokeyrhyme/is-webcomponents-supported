b0b325c21ed12c520739dbb501fb9dcde9085f17
Ron Waldon <jokeyrhyme@gmail.com>
2017-03-20 20:15:06 +1100

    run @jokeyrhyme/node-init

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-'use strict';
+'use strict'
 
 /**
  * @param {`Document`} [doc] a DOM Document to run tests against
  * @returns {`Boolean`} does this JavaScript environment support WebComponents?
  */
 module.exports = function (doc) {
-  doc = doc || global.document;
+  doc = doc || global.document
   return !!(
     doc &&
     'registerElement' in doc &&
     'import' in doc.createElement('link') &&
     'content' in doc.createElement('template')
-  );
-};
+  )
+}
